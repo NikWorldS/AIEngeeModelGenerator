16ebcd26067ae48115aac95ed813c0a4d86d08cb
Nikolai <kvoronin50@yandex.ru>
2026-05-24 20:49:51 +0500

    Update qdrant_restore.py: - Change restoring to "post" request (restore from qdrant isn't working with uploaded file)

--- app/vector_db/qdrant_restore.py
+++ app/vector_db/qdrant_restore.py
@@ -1,6 +1,10 @@
 from qdrant_client import QdrantClient
 from pathlib import Path
-import os
+import requests
+
+from app.core.config import get_settings
+
+settings = get_settings()
 
 
 QDRANT_SNAPSHOT_PATH = "qdrant_snapshots"
@@ -9,7 +13,10 @@ class QdrantRestore:
     def __init__(self, client: QdrantClient, collection_name: str):
         self.client = client
         self.collection_name = collection_name
-        self.snapshots_dir = Path(QDRANT_SNAPSHOT_PATH).resolve()
+
+        self.url = f"{settings.qdrant_url}/collections/{self.collection_name}/snapshots/upload"
+        self.snapshots_dir = Path(QDRANT_SNAPSHOT_PATH)
+
 
     def snapshot_exists_in_folder(self) -> bool:
         return self.snapshots_dir.exists() and any([p for p in self.snapshots_dir.iterdir() if p.is_file() and p.suffix == ".snapshot"])
@@ -17,7 +24,11 @@ class QdrantRestore:
     def restore_from_snapshot(self):
         snapshots = [p for p in self.snapshots_dir.iterdir() if p.is_file() and p.suffix == ".snapshot"]
         target_snapshot = max(snapshots, key=lambda p: p.stat().st_ctime)
-        return self.client.recover_snapshot(
-            self.collection_name,
-            str(target_snapshot),
+        files = {"snapshot": open(target_snapshot, "rb")}
+
+        response = requests.post(
+            self.url,
+            files=files,
         )
+        return response.json().get("result")
+
